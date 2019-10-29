d1bea14aea549c354c858848db29887ef4288053
Girma Nigusse <xgirma@gmail.com>
2019-10-29 05:28:45 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# selenium-webdriver (es6 modules, esm, mocha, chai)
+# selenium-webdriver (ES6 modules, esm, mocha, chai)
+
 selenium-webdriver end-to-end test automation boilerplate project. Using ES6 modules, ESM, mocha and chai.
+
+## Getting Started
+
+    git clone git@github.com:e2e-boilerplates/selenium-webdriver-es6modules-esm-mocha-chai.git
+    cd selenium-webdriver-es6modules-esm-mocha-chai
+    npm install
+    npm run test
---
+++ spec/search.spec.js
@@ -0,0 +1,40 @@
+import { expect } from "chai";
+import { Builder, By, Key, until } from "selenium-webdriver";
+
+import "chromedriver";
+
+describe("google Search", () => {
+  let browser;
+
+  before(async () => {
+    browser = await new Builder().forBrowser("chrome").build();
+    browser.get("https://www.google.com");
+  });
+
+  after(() => {
+    browser.quit();
+  });
+
+  it("should be on google search page", async () => {
+    const searchBox = await browser.findElement(By.name("q"));
+    await browser.wait(until.elementIsVisible(searchBox), 5000);
+
+    const title = await browser.getTitle();
+    expect(title).to.equal("Google");
+  });
+
+  it("should search for Cheese!", async () => {
+    const searchBox = await browser.findElement(By.name("q"));
+    // eslint-disable-next-line no-unused-expressions
+    expect(await searchBox.isDisplayed()).to.be.true;
+    searchBox.sendKeys("Cheese!", Key.ENTER);
+  });
+
+  it('the page title should start with "Cheese!"', async () => {
+    await browser.wait(until.urlContains("search"), 5000);
+
+    const title = await browser.getTitle();
+    const words = title.split(" ");
+    expect(words[0]).to.equal("Cheese!");
+  });
+});
